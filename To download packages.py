3228fbc02098d9232bcee614956ebe6084cc7563
niall1994 <74799033+niall1994@users.noreply.github.com>
2021-02-07 13:24:07 +0100

    Update To download packages.py

--- To download packages.py
+++ To download packages.py
@@ -1,1 +1,5 @@
 /Library/Frameworks/Python.framework/Versions/3.8/bin/python3.8 -m pip install matplotlib
+
+mkdir avocado_analytics && cd avocado_analytics 
+ python3 -m venv venv 
+source venv/bin/activate 
