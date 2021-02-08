13c49abe33092bd3dc41d2374f84b4ffc6495eab
niall1994 <74799033+niall1994@users.noreply.github.com>
2021-02-08 19:27:13 +0100

    Update Cheat Sheet.py

--- Cheat Sheet.py
+++ Cheat Sheet.py
@@ -1,6 +1,10 @@
 /Library/Frameworks/Python.framework/Versions/3.8/bin/python3.8 -m pip install matplotlib
 
+mkdir new_folder && cd new_folder
+python3.8 -m venv venv 
 python3.8 -m venv /Users/niallsheppard/virtual_environment
+
+
 
 (base) niallsheppard@Nialls-Air ~ % source /Users/niallsheppard/virtual_environment/bin/activate
 (virtual_environment) (base) niallsheppard@Nialls-Air ~ % which python
